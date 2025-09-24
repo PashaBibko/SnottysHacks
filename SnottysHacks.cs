d77d11dc8f8dfa36e9a138f8a4454c1f0262d943
Pasha Bibko <156938226+PashaBibko@users.noreply.github.com>
2025-09-24 14:36:23 +0100

    Initial commit

---
+++ SnottysHacks.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using BepInEx;
+
+namespace SnottysHacks
+{
+    [BepInPlugin("com.pashabibko.snottyhacks", "snottyhacks", "1.0.0")]
+    public class SnottysHacksPlugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Logger.LogInfo("Snottys hacks have been loaded");
+        }
+    }
+}
